73d3f4b9da7c7c1ff6fcd23c5f08736e7784110d
Sonia Garcia Martinez <garmarsonia@gmail.com>
2020-02-09 18:24:14 +0100

    home-page

---
+++ components/Blob.vue
@@ -0,0 +1,37 @@
+<template>
+   <svg class="svg-blob"  viewBox="0 0 600 600" xmlns="http://www.w3.org/2000/svg">
+    <g transform="translate(230, 200)">
+        <path
+          d="M184.3,-110.4C211,-60.2,185.6,16.2,146.2,64.9C106.8,113.7,53.4,134.8,-11.5,141.5C-76.5,148.2,-153,140.3,-192.4,91.6C-231.8,42.8,-234.1,-46.8,-195.9,-103.7C-157.6,-160.7,-78.8,-184.8,0,-184.8C78.8,-184.8,157.6,-160.7,184.3,-110.4Z"
+          fill="rgba(255, 66, 101, 1)"
+          stroke="none"
+          stroke-width="0">
+        </path>
+    </g>
+  </svg>
+</template>
+
+<style lang = "scss">
+  @import '../assets/styles/main.scss';
+
+ .svg-blob{
+    width: 200px;
+    height: 200px;
+     -webkit-animation:  blob-animation ease-in-out 12s infinite;
+   -moz-animation: my blob-animationopright ease-in-out 12s infinite;
+   animation:  blob-animation ease-in-out 12s infinite;
+ }
+
+
+ @keyframes blob-animation {
+  0% {
+		transform: translatey(0px) scale(1);
+	}
+	50% {
+		transform: translatey(-15px) scale(1.05);
+	}
+	100% {
+		transform: translatey(0px) scale(1);
+	}
+ }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,75 +1,173 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-       {{ $t('header.title') }}
-      </h1>
-      <h2 class="subtitle">
-        My splendiferous Nuxt.js project
-      </h2>
-          <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
+  <div class="container-fuid waves" id="header" >
+    <div class="d-flex align-items-center justify-content-center h-100 inner-wrapper">
+      <!-- to give it another width that the default 200px use
+           <blob :style="{ width: '100px' }"/>  -->
+        <div class="header-msg">
+           <p class="greeting">
+              <blob class="blob" />
+              Hi, I'm Sonia. <br>
+              <span class="smaller">
+                I code. <br>
+                I design. <br>
+                I solve problems. <br>
+              </span>
+            </p>
 
+            <p class="message">
+              I collaborate with companies and teams to create first class web and mobile apps
+              that add value to their business.
+              <br> <br>
+              If what is already in the market doesn't fit your needs, I'll create it for your.
+              <br>  <br>
+              <span class="underline"> You know what you need, I know how to build it. </span>
+            </p>
 
-
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+            <div class="d-flex justify-content-center">
+                <button type="button" class="btn button--default">Let's work together</button>
+            </div>
+        </div>
     </div>
+
+    <svg class="svg-waves"
+    viewBox="0 24 150 28 "
+    preserveAspectRatio="none">
+    <defs>
+    <path id="wave"
+    d="M-160 44c30 0
+      58-18 88-18s
+      58 18 88 18
+      58-18 88-18
+      58 18 88 18
+      v44h-352z" />
+      </defs>
+        <g class="wave-one">
+            <use xlink:href="#wave" x="50" y="0" fill="#ff4265"/>
+        </g>
+
+        <g class="wave-two">
+              <use xlink:href="#wave" x="50" y="4" fill="#ecf0f1"/>
+        </g>
+    </svg>
+
+
+
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Blob from '~/components/Blob.vue'
 
 export default {
   components: {
-    Logo
+    Blob
   }
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+<style lang = "scss">
+  @import '../assets/styles/main.scss';
+
+#header{
+   background-color: $black;
+   & .inner-wrapper {
+     padding: 0 15%;
+     & .header-msg {
+      margin-bottom: 70px;
+      font-family: 'IBM Plex Mono', monospace;
+      color: $white;
+       & .greeting{
+        position: relative;
+        font-size: 4rem;
+        line-height: 4rem;
+        z-index: 5;
+        .smaller {
+          font-size: 3rem;
+          line-height: 3rem;
+        }
+        .special {
+          color: $base-color;
+        }
+      }
+
+      & .message {
+        font-family: 'IBM Plex Mono', monospace;
+        color: $white;
+        font-size: 2rem;
+        line-height: 2.2rem;
+        margin-top: 50px;
+      }
+
+       & .blob {
+        position: absolute;
+        top: -25px;
+        z-index: -1;
+        left: -95px;
+      }
+   }
+   }
+
+
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+
+.underline{
+  text-decoration: underline;
+   text-decoration-color: $base-color;
+}
+
+.waves{
+  display:block;
+  position: relative;
+  width: 100%;
+  height: calc(100vh - 100px);
+  background: $black;
+  & > .svg-waves{
   display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+    width: 100%;
+    height: 60px;
+    max-height: 60px;
+    margin: 0;
+    z-index:5;
+    bottom:0;
+    position:absolute;
+    left:0px;
+    float:left;
+  }
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.wave-one > use {
+  animation: move-forever2 8s linear infinite;
+  &:nth-child(1) {
+    animation-delay: -2s;
+  }
 }
 
-.links {
-  padding-top: 15px;
+.wave-two > use {
+  animation: move-forever4 4s linear infinite;
+  &:nth-child(1) {
+    animation-delay: -2s;
+  }
 }
+
+
+@keyframes move-forever4 {
+  0% {
+    transform: translate(-90px, 0%);
+  }
+  100% {
+    transform: translate(85px, 0%);
+  }
+}
+
+
+@keyframes move-forever2 {
+  0% {
+    transform: translate(-90px, 0%);
+  }
+  100% {
+    transform: translate(85px, 0%);
+  }
+}
+
 </style>
